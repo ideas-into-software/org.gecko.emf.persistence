974b58efa805e140005cacd325238d20d98d756b
Mark Hoffmann <m.hoffmann@data-in-motion.biz>
2024-01-12 12:39:56 +0100

    Signed-off-by: Mark Hoffmann <m.hoffmann@data-in-motion.biz>

--- org.gecko.emf.repository.mongo/src/org/gecko/emf/repository/mongo/package-info.java
+++
@@ -1,7 +0,0 @@
-@org.osgi.annotation.bundle.Capability(
-		namespace = org.gecko.emf.persistence.PersistenceConstants.CAPABILITY_NAMESPACE,
-		name = "mongo",
-		version = "2.2"
-		)
-@org.osgi.annotation.versioning.Version("2.2.0")
-package org.gecko.emf.repository.mongo;
